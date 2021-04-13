bec7bb3d9688065f39824ef4f68abe3a70cfe39a
Guilherme <55421308+gaia332@users.noreply.github.com>
2021-04-13 16:12:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
-server.get('/', function(request, response) {
-    response.send('Hello World!');
+server.use(cors());
+server.use(express.json());
+
+contatos = [];
+
+server.get('/', async function(request, response) {
+    return response.json(contatos);
+})
+
+server.get('/:id' function(request, response) { 
+    const nome = request.body.nome; // JSON
+    const quantidade = request.body.numero;
+
+    return response.status(204).send();
 })
 
 server.listen(process.env.PORT || 3000);
